f9c336e2ba940e5488b3b7107301739a49476eae
hayabusabusa <hayabusabusaSY@gmail.com>
2021-06-18 00:21:59 +0900

    chore: commit for CI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 
-const a = 'commit for CI';
+const a = 'commit for CI2';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
