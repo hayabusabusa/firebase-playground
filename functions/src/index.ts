4a8100efc3af336fbbcb92a9ccbee2b249c4c1d7
hayabusabusa <hayabusabusaSY@gmail.com>
2021-06-18 23:25:05 +0900

    chore: commit for CI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as functions from 'firebase-functions';
 const a = "commit for CI";
 const b = "commit for CI";
 const c = "commit for CI";
+const d = "commit for CI";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
